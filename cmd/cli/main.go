1c44f04fc9688c29793e827d45506482a11bb9f7
Kevin Zheng <me@kzh.dev>
2021-11-07 01:22:53 -0500

    Initial commit

---
+++ cmd/cli/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "arcanecli",
+	Run: func(cmd *cobra.Command, args []string) {
+	},
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
