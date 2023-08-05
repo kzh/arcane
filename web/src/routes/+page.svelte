36e21d672d82ee239c12159aaf49dce040d5bfaf
kevin <me@kzh.dev>
2023-08-05 03:24:44 -0400

    web: add barebone setup

---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<div class="container flex h-screen justify-center items-center">
+    <h1 class="text-3xl text-center font-bold">
+      Arcane
+    </h1>
+</div>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
